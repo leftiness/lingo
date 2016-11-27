d8621426c15d5a8f95889353da730f7d7243be27
Brandon Parmenter <leftiness@gmail.com>
2016-11-26 23:12:27 -0500

    Issue #30: Added basic main process

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+extern crate lingo;
+
+use std::thread;
+
+use lingo::config::Loader;
+use lingo::event::{Broadcaster, Event, Logger, Dispatcher};
+
+/// Start Lingo
+pub fn main() {
+
+  let mut dispatcher = Dispatcher::new();
+
+  let mut logger = Logger::with_dispatcher(dispatcher.tx().clone());
+  let mut loader = Loader::with_dispatcher(dispatcher.tx().clone());
+
+  let tx = dispatcher.tx().clone();
+
+  dispatcher.register(logger.tx().clone());
+  dispatcher.register(loader.tx().clone());
+
+  let process = thread::spawn(move || dispatcher.listen());
+
+  thread::spawn(move || logger.listen());
+  thread::spawn(move || loader.listen());
+
+  tx.send(Event::LoadPreference).unwrap();
+
+  process.join().unwrap();
+
+}
+
